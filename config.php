ecd61a81202af9c7cd9de1578a21170b9ec742ae
stonecjn <89263041+stonecjn@users.noreply.github.com>
2022-05-08 14:05:08 -0500

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//1 = yes; 0 = no;
-$send = "youremail@gmail.com";
-$log = 1;
-$onetime = 1; //One Time Access: This blocks the users ip after the form has been submitted i.e. prevents users sending multiple fake forms
-?>
